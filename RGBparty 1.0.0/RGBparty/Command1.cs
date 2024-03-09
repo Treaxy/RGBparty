dfdca4b166fb97ceb48266e0df7233c64a2c3811
Banalny-Banan <133122450+Banalny-Banan@users.noreply.github.com>
2024-03-09 23:02:21 +0200

    Update Command1.cs
    Reworked the command

--- RGBparty 1.0.0/RGBparty/Command1.cs
+++ RGBparty 1.0.0/RGBparty/Command1.cs
@@ -10,8 +10,7 @@ namespace RGBparty
     internal class ConsoleCommand
     {
         [CommandHandler(typeof(RemoteAdminCommandHandler))]
-
-        public class RGBparty : ICommand
+        public class RgbParty : ICommand, IUsageProvider
         {
             public string Command => "RGBparty";
 
@@ -19,37 +18,65 @@ namespace RGBparty
 
             public string Description => "Disco!! Party!!";
 
+            public string[] Usage { get; } = { "Duration", "Speed" };
+
             public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
             {
-
                 if (!sender.CheckPermission("rgbparty"))
                 {
-                    response = null;
+                    response = "You don't have permission to use this command! (rgbparty)";
                     return false;
+                }
+
+                var duration = 30f;
+                if (arguments.Count >= 1 && float.TryParse(arguments.At(0), out float newDuration))
+                {
+                    duration = newDuration;
+                }
+                
+                var lerpSpeed = 2.5f;
+                if (arguments.Count >= 2 && float.TryParse(arguments.At(1), out float newLerpSpeed))
+                {
+                    lerpSpeed = newLerpSpeed;
                 }
 
                 Map.Broadcast(5, Plugin.Instance.Config.PartyStartMessage);
                 Log.Info("Party Started!");
                 Cassie.Message(Plugin.Instance.Config.PartyStartCassie, true, true, true);
-                float count = 0f;
-                for (; ; )
+                Party(duration, lerpSpeed).RunCoroutine();
+                response = $"Party Started for {duration} seconds!";
+                return true;
+            }
+
+            public static IEnumerator<float> Party(float duration, float lerpSpeed)
+            {
+                var colors = new List<Color>
                 {
-                    if (count > 30f)
-                    {
-                        break;
-                    }
-                    Timing.CallDelayed(count + 1f, () => { Map.ChangeLightsColor(Color.red); });
-                    Timing.CallDelayed(count + 2f, () => { Map.ChangeLightsColor(Color.yellow); });
-                    Timing.CallDelayed(count + 3f, () => { Map.ChangeLightsColor(Color.cyan); });
-                    Timing.CallDelayed(count + 4f, () => { Map.ChangeLightsColor(Color.magenta); });
-                    Timing.CallDelayed(count + 5f, () => { Map.ChangeLightsColor(Color.green); });
-                    Timing.CallDelayed(count + 6f, () => { Map.ChangeLightsColor(Color.blue); });
-                    Timing.CallDelayed(count + 7f, () => { Map.ChangeLightsColor(Color.white); });
-                    count += 7f;
+                    new Color(1, 0.2f, 0.2f), //default red is dim
+                    new Color(1, 0.5f, 0), //orange
+                    Color.yellow,
+                    Color.green*1.1f,
+                    Color.cyan,
+                    new Color(0F, 0.65F, 1.3F), //nice blue
+                    new Color(0.78F, 0.13F, 1.3F), //purple
+                };
+
+                var currentColor = Color.white;
+
+                //lerp between colors
+                for (float i = 0; i < duration * lerpSpeed; i += 0.05f)
+                {
+                    currentColor = Color.Lerp(colors[(int)(i % colors.Count)], colors[(int)((i + 1) % colors.Count)], i % 1);
+                    Map.ChangeLightsColor(currentColor * 1.5f);
+                    yield return Timing.WaitForSeconds(0.05f / lerpSpeed);
                 }
 
-                response = "Party Started!!";
-                return true;
+                //lerp back to white
+                for (float i = 0; i < 1; i += 0.1f)
+                {
+                    Map.ChangeLightsColor(Color.Lerp(currentColor * 1.5f, Color.white, i));
+                    yield return Timing.WaitForSeconds(0.1f);
+                }
             }
         }
     }
